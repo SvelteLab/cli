a6931d2d7bed3b660265846727c5bb04158f0cdc
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-07-07 10:02:40 +0200

    Save command basically done

--- src/commands/save.ts
+++ src/commands/save.ts
@@ -1,20 +1,74 @@
+type File = { file: { contents: Uint8Array } };
+
+type Directory = {
+	directory: {
+		[filename: string]: File | Directory;
+	};
+};
+
+function get_files(project: any) {
+	let retval: Directory = { directory: {} };
+	for (let file_name in project) {
+		const file = project[file_name];
+		if ('directory' in file) {
+			retval.directory[file_name] = get_files(file.directory);
+		} else {
+			console.log(file_name, file);
+			retval.directory[file_name] = {
+				file: { contents: new Uint8Array(file.file.contents.buffer) },
+			};
+		}
+	}
+	return retval;
+}
+
+async function write_files(files: Directory, base_path: string) {
+	for (let file_name in files.directory) {
+		const file = files.directory[file_name];
+		if ('directory' in file) {
+			try {
+				await Deno.mkdir(`${base_path}/${file_name}`, {
+					recursive: true,
+				});
+			} catch (_e) {
+				await Deno.remove(`${base_path}/${file_name}`, {
+					recursive: true,
+				});
+				await Deno.mkdir(`${base_path}/${file_name}`, {
+					recursive: true,
+				});
+			}
+			write_files(file, `${base_path}/${file_name}`);
+		} else {
+			await Deno.writeFile(
+				`${base_path}/${file_name}`,
+				file.file.contents,
+				{
+					create: true,
+				},
+			);
+		}
+	}
+}
+
 export async function save(url: string, destination: string) {
-	const actual_url = new URL(url);
-	const is_correct_url = ['sveltelab.dev', 'www.sveltelab.dev'].includes(
-		actual_url.hostname,
-	);
+	const actual_url = new URL(`${url}.json`);
+	const is_correct_url = [
+		'sveltelab.dev',
+		'www.sveltelab.dev',
+		'localhost',
+	].includes(actual_url.hostname);
 	if (!is_correct_url) {
 		throw new Error('Invalid URL: it must be a sveltelab project');
 	}
-	const project = await fetch(actual_url).then((res) => res.text());
-	const ui8a = new Uint8Array([...project].map((char) => char.charCodeAt(0)));
+	const project = await fetch(actual_url).then((res) => res.json());
 	try {
 		await Deno.mkdir(destination, { recursive: true });
 	} catch (_e) {
 		await Deno.remove(destination, { recursive: true });
 		await Deno.mkdir(destination, { recursive: true });
 	}
-	await Deno.writeFile(`${destination}/file`, ui8a, {
-		create: true,
-	});
+	const files = get_files(project.files);
+
+	await write_files(files, destination);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { save } from './commands/save.ts';
-import { Command } from './deps.ts';
+import { Command } from './deps/commander.ts';
 
 const program = new Command('SvelteLab CLI');
 
-program.command('save <url> [destination]').action((url, destination) => {
-	save(url, destination);
-});
+program
+	.command('save <url> [destination]')
+	.description(
+		'save a SvelteLab REPL specified by <url> to the [destination] folder',
+	)
+	.action((url: string, destination: string) => {
+		save(url, destination);
+	});
 
 program.command('load').action(() => {});
 
